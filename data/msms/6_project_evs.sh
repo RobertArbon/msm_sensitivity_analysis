b6446d82815a969eb3126f4c8b8a5ddf86b7ba08
robarbon <robert.arbon@gmail.com>
2022-03-04 11:23:21 +0000

    run scripts for ev projections

--- data/msms/6_project_evs.sh
+++ data/msms/6_project_evs.sh
@@ -3,37 +3,34 @@
 export PYEMMA_NJOBS=1
 export OMP_NUM_THREADS=1
 
-#hps=(53 52 60 47 86 81)
+#@cli.command()
 #@click.option('-l', '--lag', type=int, help='Lag of model')
-#@click.option('-k', '--processes', type=int, help='Number of process to compare')
-#@click.option('-q', '--num-cuts', type=int, help='Number of quantiles to cut the EV into')
 #@click.option('-i', '--hp-sample', type=Path, help='Path to file that contains the hyperparameter samples')
 #@click.option('-d', '--data-dir', type=Path, help='Base directory used to determine trajectory and topology paths')
 #@click.option('-t', '--topology-path', type=Path, help='Topology path')
 #@click.option('-g', '--trajectory-glob', type=str, help='Trajectory glob string relative to --data-dir')
-#@click.option('-r', '--num-repeats', type=int, help='Number of bootstrap samples')
 #@click.option('-n', '--num-cores', type=int, help='Number of cpu cores to use.', default=1)
 #@click.option('-o', '--output-dir', type=Path, help='Path to output directory')
 #@click.option('-s', '--seed', type=int, help='Random seed', default=None)
-#@click.argument('hp-ixs', type=int, nargs=-1)
-states=2
-n_cuts=50
+#@click.option('-h', '--hp-ix', type=int, help='HP index to model')
+#@click.option('-p', '--project-dir', type=Path, help='Directory containing the ev projections')
+#@click.argument('project-ixs', type=int, nargs=-1)
+
 lag=41
-n_samples=2
 n_cores=2
 
-msmsense sample-evs \
+msmsense project-evs \
   -l ${lag} \
-  -k ${states} \
-  -q ${n_cuts} \
   -i hpsample.h5 \
   -d /home/rob/Data/DESRES \
   -t  /home/rob/Data/DESRES/DESRES-Trajectory_1FME-0-protein/1FME-0-protein/protein.pdb \
   -g '*1FME*/**/*.xtc' \
-  -r ${n_samples} \
   -n ${n_cores} \
-  -o 1fme/evs \
+  -o 1fme/project_evs \
   -s 49587 \
-  53 52 60 47 86 81
+  -h 53 \
+  -p 1fme/evs \
+  52 53 47 60 81 86
+
 
 #  52 60 47 86 81
